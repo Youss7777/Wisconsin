28f19a34a4600086ab5fc2a059310ea1c7e9a12d
Youss7777 <150968085+Youss7777@users.noreply.github.com>
2024-11-27 23:16:30 +0100

    Add files via upload

--- WSCT_model5_exp.m
+++ WSCT_model5_exp.m
@@ -3,25 +3,52 @@
 clear all
 
 
-rng('shuffle')
+%rng('default')
 
 % Number of time steps per trial
 T = 3;
+% Source deck
+deck{1} = {[0, 0, 0, 1]', [0, 0, 0, 1]', [0, 0, 0, 1]'};    % star, yellow, 4
+deck{2} = {[0, 0, 1, 0]', [1, 0, 0, 0]', [1, 0, 0, 0]'};    % rectangle, blue, 1
+deck{3} = {[1, 0, 0, 0]', [0, 1, 0, 0]', [0, 1, 0, 0]'};    % cicle, red, 2
+deck{4} = {[0, 1, 0, 0]', [0, 0, 1, 0]', [0, 0, 1, 0]'};    % triangle, green, 3
+deck{5} = {[0, 0, 1, 0]', [1, 0, 0, 0]', [0, 1, 0, 0]'};    % rectangle, blue, 2
+deck{6} = {[1, 0, 0, 0]', [0, 1, 0, 0]', [0, 0, 1, 0]'};    % circle, red, 3
+deck{7} = {[0, 1, 0, 0]', [0, 0, 1, 0]', [0, 0, 0, 1]'};    % triangle, green, 4
+deck{8} = {[0, 0, 0, 1]', [0, 0, 0, 1]', [1, 0, 0, 0]'};    % star, yellow, 1
+deck{9} = {[0, 0, 1, 0]', [0, 0, 1, 0]', [0, 0, 0, 1]'};    % rectangle, green, 4
+deck{10} = {[1, 0, 0, 0]', [0, 0, 0, 1]', [1, 0, 0, 0]'};   % circle, yellow, 1
+deck{11} = {[0, 1, 0, 0]', [1, 0, 0, 0]', [0, 1, 0, 0]'};   % triangle, blue, 2
+deck{12} = {[0, 0, 0, 1]', [0, 1, 0, 0]', [0, 0, 1, 0]'};   % star, red, 3
+deck{13} = {[1, 0, 0, 0]', [0, 0, 0, 1]', [0, 1, 0, 0]'};   % circle, yellow, 2
+deck{14} = {[0, 1, 0, 0]', [1, 0, 0, 0]', [0, 0, 1, 0]'};   % triangle, blue, 3
+deck{15} = {[0, 0, 0, 1]', [0, 1, 0, 0]', [0, 0, 0, 1]'};   % star, red, 4
+deck{16} = {[0, 0, 1, 0]', [0, 0, 1, 0]', [1, 0, 0, 0]'};   % rectangle, green, 1
+deck{17} = {[0, 0, 0, 1]', [1, 0, 0, 0]', [0, 0, 1, 0]'};   % star, blue, 3
+deck{18} = {[0, 0, 1, 0]', [0, 1, 0, 0]', [0, 0, 0, 1]'};   % rectangle, red, 4
+deck{19} = {[1, 0, 0, 0]', [0, 0, 1, 0]', [1, 0, 0, 0]'};   % circle, green, 1
+deck{20} = {[0, 1, 0, 0]', [0, 0, 0, 1]', [0, 1, 0, 0]'};   % triangle, yellow, 2
+deck{21} = {[1, 0, 0, 0]', [0, 0, 1, 0]', [0, 0, 1, 0]'};   % circle, green, 3
+deck{22} = {[0, 1, 0, 0]', [0, 0, 1, 0]', [0, 0, 0, 1]'};   % triangle, yellow, 4
+deck{23} = {[0, 0, 0, 1]', [1, 0, 0, 0]', [1, 0, 0, 0]'};   % star, blue, 1
+deck{24} = {[0, 0, 1, 0]', [0, 1, 0, 0]', [0, 1, 0, 0]'};   % rectangle, red, 2
+
+% Target cards
 % Card 1
-Card{2}{1} = [1, 0, 0, 0]';       % shape = circle
-Card{2}{2} = [1, 0, 0, 0]';       % color = blue
+Card{2}{1} = [0, 1, 0, 0]';       % shape = triangle
+Card{2}{2} = [0, 1, 0, 0]';       % color = red
 Card{2}{3} = [1, 0, 0, 0]';       % number = 1
 % Card 2
-Card{3}{1} = [0, 1, 0, 0]';       % shape = triangle
-Card{3}{2} = [0, 1, 0, 0]';       % color = red
+Card{3}{1} = [0, 0, 0, 1]';       % shape = star
+Card{3}{2} = [0, 0, 1, 0]';       % color = green
 Card{3}{3} = [0, 1, 0, 0]';       % number = 2
 % Card 3
-Card{4}{1} = [0, 0, 1, 0]';       % shape = square
-Card{4}{2} = [0, 0, 1, 0]';       % color = green
+Card{4}{1} = [0, 0, 1, 0]';       % shape = rectangle
+Card{4}{2} = [0, 0, 0, 1]';       % color = yellow
 Card{4}{3} = [0, 0, 1, 0]';       % number = 3
 % Card 4
-Card{5}{1} = [0, 0, 0, 1]';       % shape = star
-Card{5}{2} = [0, 0, 0, 1]';       % color = yellow
+Card{5}{1} = [1, 0, 0, 0]';       % shape = circle
+Card{5}{2} = [1, 0, 0, 0]';       % color = blue
 Card{5}{3} = [0, 0, 0, 1]';       % number = 4
 
 
@@ -47,7 +74,7 @@ D{6} = [1, 0, 0, 0, 0]';      % choice = wait
 d{1} = [0.25, 0.25, 0.25, 0.25]';
 d{2} = [0.25, 0.25, 0.25, 0.25]';
 d{3} = [0.25, 0.25, 0.25, 0.25]';
-d{4} = [1, 1, 1, 0.05]';       % low prior for the exclusion rule
+d{4} = [1, 1, 1, 0.5]';       % low prior for the exclusion rule
 d{5} = [1, 0, 0]';
 d{6} = [1, 0, 0, 0, 0]';
 
@@ -175,14 +202,15 @@ A{5}(3, :, :, :, :, 2, :) = 1;              % response sequence
 
 % PREFERRED OUTCOMES
 % ------------------------------------------
-la = 1;
-rs = 4;
+loss = 1;
+reward = 4;
 for i=1:Ng
     C{i} = zeros(No(i), T);
 end
-C{Ng}(:, T) = [-la; % Incorrect
-                rs;  % Correct
+C{Ng}(:, T) = [-loss; % Incorrect
+                reward;  % Correct
                 0]; % Null
+
 
 % Additional parameters
 % ---------------------------------------------
@@ -210,7 +238,8 @@ mdp.eta = eta;                % learning rate
 mdp.omega = omega;            % forgetting rate
 mdp.alpha = alpha;            % action precision
 mdp.beta = beta;              % expected precision of expected free energy over policies
-
+mdp.loss = loss;
+mdp.reward = reward;
 
 % We can add labels to states, outcomes, and actions for subsequent plotting:
 label.factor{1}   = 'shape';
@@ -241,78 +270,89 @@ for i = 1:Nf
 end
 mdp.label = label;
 
-clear beta
-clear alpha
-clear eta
-clear omega
-clear la
-clear rs % We clear these so we can re-specify them in later simulations
-
 
 % Multiple trials simulation
-N = 60; % number of trials
+N = 81; % number of trials
 
 MDP = mdp;
 
 [MDP(1:N)] = deal(MDP);
 
-% Changing features
+% Draw new source card randomly from deck
 for i=1:N
+    rand_idx = randi([1, size(deck, 2)]);
     for feature=1:3
-        MDP(i).D{feature} = zeros(Ns(1), 1);
-        rand_idx = randi([1, 4]);
-        MDP(i).D{feature}(rand_idx) = 1;
+        MDP(i).D{feature} = deck{rand_idx}{feature};
     end
 end
 
 
+% % Model reduction at a specific trial
+% BMR1.f = 4;
+% BMR1.x = 1;
+% BMR1.trial = 15;
+% OPTIONS.BMR1 = BMR1;
 
-BMR1.f = 4;
-BMR1.x = 1;
-BMR1.trial = 30;
-OPTIONS.BMR1 = BMR1;
-MDP = spm_MDP_VB_X_tutorial(MDP);
 
+% MODEL INVERSION
+%==============================================================
+DCM.MDP = mdp;
+MDP = WSCT_get_empirical_obs_act(MDP);
+DCM.U = {MDP.o};
+DCM.Y = {MDP.u};
+DCM.field = {'alpha', 'eta', 'reward', 'loss'};
 
-f_act = Nf;
-f_state = 4;
-mod_out = Ng;
-timestep_to_plot = 2;
+DCM = WSCT_Estimate_parameters(DCM); % Run the parameter estimation function
 
-WSCT_plot(MDP, f_act, f_state, mod_out, timestep_to_plot);
+subplot(2,2,3)
+xticklabels(DCM.field),xlabel('Parameter')
+subplot(2,2,4)
+xticklabels(DCM.field),xlabel('Parameter')
+
+field = fieldnames(DCM.M.pE);
+for i = 1:length(field)
+    if strcmp(field{i},'eta')
+        prior(i) = 1/(1+exp(-DCM.M.pE.(field{i})));
+        posterior(i) = 1/(1+exp(-DCM.Ep.(field{i}))); 
+    elseif strcmp(field{i},'omega')
+        prior(i) = 1/(1+exp(-DCM.M.pE.(field{i})));
+        posterior(i) = 1/(1+exp(-DCM.Ep.(field{i})));
+    else
+        prior(i) = exp(DCM.M.pE.(field{i}));
+        posterior(i) = exp(DCM.Ep.(field{i}));
+    end
+end
+
+figure, set(gcf,'color','white')
+subplot(2,1,1),hold on
+title('Means')
+bar(prior, 0.5, 'FaceColor',[.5,.5,.5]), bar(posterior,1, 0.5,'k')
+xlim([0,length(prior)+1]),set(gca, 'XTick', 1:length(prior)),set(gca, 'XTickLabel', DCM.field)
+legend({'Prior','Posterior'})
+hold off
+subplot(2,1,2)
+imagesc(DCM.Cp),caxis([0 1]),colorbar
+title('(Co-)variance')
+set(gca, 'XTick', 1:length(prior)),set(gca, 'XTickLabel', DCM.field)
+set(gca, 'YTick', 1:length(prior)),set(gca, 'YTickLabel', DCM.field)
+ 
+
+% % Solve
+%MDP = spm_MDP_VB_X(MDP);
+% 
+% % Plotting parameters
+% f_act = Nf;
+% f_state = 4;
+% mod_out = Ng;
+% timestep_to_plot = 2;
+% 
+% % Plot
+% WSCT_plot(MDP, f_act, f_state, mod_out, timestep_to_plot);
 
 
 % % Changing rule
 % for i=10:20
 %     MDP(i).D{4} = [0 0 1 0]';    % rule = number
 % end
-
-% % Model reduction
-% MDP2 = MDP(end);
-% [sd, rd] = WSCT_prune(MDP(end).d{4}, MDP(1).d_0{4}, 8);
-% MDP2.d{4} = sd;
-% MDP2.d0{4} = rd;
-% 
-% N2 = 20;
-% [MDP2(1:N2)] = deal(MDP2);
-% 
-% for i=1:N2
-%     for feature=1:3
-%         MDP2(i).D{feature} = zeros(Ns(1), 1);
-%         rand_idx = randi([1, 4]);
-%         MDP2(i).D{feature}(rand_idx) = 1;
-%     end
-% end
-% 
-% 
-% MDP2 = spm_MDP_VB_X_tutorial(MDP2);
-% 
-% for i = 1:N1-1
-%     SIM(i) = MDP(i);
-% end
-% for i = 1:N2
-%     SIM(i+N1-1) = MDP2(i);
-% end
-
 
 
